88a7dfba4bab2c236f549ddb04182b2ef16edfb8
saiwing <6488798+dzgogo@users.noreply.github.com>
2024-05-31 15:40:58 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -8,6 +8,9 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#echo 'src-git oui https://github.com/zhaojh329/oui.git' >>feeds.conf.default
 
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
